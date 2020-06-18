9b1a2c075d7b18f3294777144dbcb93fc5c6b349
mklmng <mikelmonge@gmail.com>
2020-06-18 19:33:00 +0100

    Updates

---
+++ precache-manifest.5634a711232ef2e0f4d4cb51cc8bf462.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "157fa5d2e2d5a83a7a028af9d71ee7e9",
+    "url": "/depop-test/index.html"
+  },
+  {
+    "revision": "83b37e7248c40e9cd5f9",
+    "url": "/depop-test/static/css/main.760631ee.chunk.css"
+  },
+  {
+    "revision": "ad20e0eb7203cb13ed0e",
+    "url": "/depop-test/static/js/2.e3f25ab8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/depop-test/static/js/2.e3f25ab8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "83b37e7248c40e9cd5f9",
+    "url": "/depop-test/static/js/main.99ab5635.chunk.js"
+  },
+  {
+    "revision": "5679e8b153e56c51b850",
+    "url": "/depop-test/static/js/runtime-main.1a3093f5.js"
+  }
+]);
